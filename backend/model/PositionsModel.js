e4ed02f7dde6e253e4a08715c73ec0bb7cea437c
Muskan <MuskanSingh698@gmail.com>
2026-01-15 13:44:04 +0530

    dashboard code

--- backend/model/PositionsModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose")
-
-const {PositonsSchema} = require('../schemas/PositionsSchema')
-
-const PositionsModel = new model("position",PositonsSchema);
-
-module.exports= {PositionsModel};
