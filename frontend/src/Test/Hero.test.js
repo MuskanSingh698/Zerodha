14bce3c47c4c1a6a6a859f39ef36f04dc4b73f67
Muskan <MuskanSingh698@gmail.com>
2026-01-15 19:58:11 +0530

    added test-file

---
+++ frontend/src/Test/Hero.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {render,screen} from '@testing-library/react';
+import "@testing-library/jest-dom";
+import Hero from "../landing_page/home/Hero";
+
+describe("Hero component",()=>{
+    test("render hero image",()=>{
+        render(<Hero/>);
+        const heroImg = screen.getByAltText("Hero Image");
+        expect(heroImg).toBeInTheDocument();
+        expect(heroImg).toHaveAttribute("src","media/images/homeHero.png")
+    });
+})
