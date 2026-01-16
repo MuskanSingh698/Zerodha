1e3909debeaa0c555f507d0a60eca9c3e15977e9
Muskan <MuskanSingh698@gmail.com>
2026-01-16 13:56:17 +0530

    fixed dashboard for deployment

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,17 +1,18 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { positions } from "../data/data";
- import axios from "axios";
+import axios from "axios";
 const Positions = () => {
 
   const [allPositions, setallPositions] = useState([]);
-    const API =process.env.REACT_APP_API_URL;
 
-useEffect(()=>{
-    axios.get(`${API}/allPositions`).then((res)=>{
+  const API = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    axios.get(`${API}/allPositions`).then((res) => {
       console.log(res.data);
       setallPositions(res.data);
     })
-  },[]);
+  }, []);
 
   return (
     <>
@@ -19,35 +20,37 @@ useEffect(()=>{
 
       <div className="order-table">
         <table>
-          <tr>
-            <th>Product</th>
-            <th>Instrument</th>
-            <th>Qty.</th>
-            <th>Avg.</th>
-            <th>LTP</th>
-            <th>P&L</th>
-            <th>Chg.</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg.</th>
+              <th>LTP</th>
+              <th>P&L</th>
+              <th>Chg.</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allPositions.map((stock, index) => {
+              const curValue = stock.price * stock.qty;
+              const isProfit = curValue - stock.avg * stock.qty >= 0.0;
+              const profClass = isProfit ? "profit" : "loss";
+              const dayClass = stock.isLoss ? "loss" : "profit";
 
-          {allPositions.map((stock,index)=>{
-            const curValue = stock.price * stock.qty;
-            const isProfit = curValue-stock.avg*stock.qty >= 0.0;
-            const profClass= isProfit? "profit" : "loss";
-            const dayClass = stock.isLoss ? "loss" : "profit";
-
-            return (
-            <tr key={index} >
-              <td>{stock.product}</td>
-            <td>{stock.name}</td>
-            <td>{stock.qty}</td>
-            <td>{stock.avg}</td>
-            <td>{stock.price}</td>
-            <td className={profClass}>{(curValue-stock.avg*stock.qty).toFixed(2)}</td>
-            <td className={dayClass}>{stock.day}</td>
-          </tr>
-            )
-          })}
-
+              return (
+                <tr key={index} >
+                  <td>{stock.product}</td>
+                  <td>{stock.name}</td>
+                  <td>{stock.qty}</td>
+                  <td>{stock.avg}</td>
+                  <td>{stock.price}</td>
+                  <td className={profClass}>{(curValue - stock.avg * stock.qty).toFixed(2)}</td>
+                  <td className={dayClass}>{stock.day}</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
       </div>
     </>
